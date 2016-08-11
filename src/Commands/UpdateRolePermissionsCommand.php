f7d095570a7ecfb69b26d0495826d462b31b5aab
Sam Thompson <sthompson@gcdtech.com>
2016-08-11 18:13:12 +0100

    command —f option and output formatting

--- src/Commands/UpdateRolePermissionsCommand.php
+++ src/Commands/UpdateRolePermissionsCommand.php
@@ -14,7 +14,7 @@ class UpdateRolePermissionsCommand extends RequiresRepositoryCommand
     {
         $this->setName('auth:update-role-permissions')
             ->setDescription('Updates Roles and Permissions according to a definitions specified in the application. Useful if the application does not allow users to customise role permissions.')
-            ->addArgument('force');
+            ->addOption('f', null, null, 'Forces update - does not prompt user');
 
         parent::configure();
     }
@@ -23,21 +23,27 @@ class UpdateRolePermissionsCommand extends RequiresRepositoryCommand
     {
         $helper = $this->getHelper('question');
 
-        $force = $input->getArgument('force') === '-f';
+        $force = $input->getOption('f');
 
+        if (!$force) {
+            $output->writeln('<comment>Warning: New roles may be created, and all permissions associated with a role may be updated/removed if not specified in RolePermissionDefinitions.</comment>');
+        }
         $verify = new Question(
-            'Warning: New roles may be created, and all permissions associated with a role may be updated/removed if not specified in RolePermissionDefinitions. Are you sure you want to proceed? [y/n] '
+            '<question>Are you sure you want to proceed? [y/n] </question>'
         );
         $verify->setValidator(function ($value) use ($output) {
             if (stripos($value, 'y') !== 0) {
-                $output->writeln('Aborted');
+                $output->writeln('<error>Aborted</error>');
                 return false;
             }
             return true;
         });
         if ($force || $helper->ask($input, $output, $verify)) {
+            if (!$force) {
+                $output->writeln('<comment>If you would like to suppress user verification in the future, use the --f option</comment>');
+            }
             AuthenticationWithRolesModule::updateRolePermissions();
-            $output->writeln('Role Permissions Updated');
+            $output->writeln('<info>Role Permissions Updated</info>');
         }
     }
 }
